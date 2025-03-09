de70cb90404ba1209abd4bab61c7f73f4d45b7a7
G30TR1X <gavriel10donovan@gmail.com>
2025-03-09 18:21:03 +0700

    Finish etch a sketch

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
+const reset = document.querySelector("#reset");
 
-for (let i = 0; i < 16; i++)
-{
-    const box = document.createElement("div")
-    box.classList.toggle("box")
+function makeBox(size) {
+  for (let i = 0; i < size * size; i++) {
+    const box = document.createElement("div");
+
+    box.style.width = `${1000 / size}px`;
+    box.style.height = `${1000 / size}px`;
+
+    box.addEventListener("mouseover", () => {
+      const red = Math.floor(Math.random() * 256);
+      const green = Math.floor(Math.random() * 256);
+      const blue = Math.floor(Math.random() * 256);
+      box.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    });
+
+    box.classList.toggle("box");
+    container.appendChild(box);
+  }
 }
+
+makeBox(16)
+
+reset.addEventListener("click", () => {
+  let grid = +prompt("How big of a grid you want?");
+
+  while (grid > 100 || grid < 1)
+    grid = prompt("Please enter a number between 1 and 100");
+
+  container.innerHTML = "";
+
+  makeBox(grid)
+});
